5dedf5c89c906dce1f37253c1f493aab99f34e1f
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-04-24 23:20:35 -0300

    feat: 🌟 now with extra pizzazz 🌟
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- app/auth.ts
+++ app/auth.ts
@@ -1,12 +1,14 @@
-import db from '@/lib/db/client';
 import type { User } from '@/prisma/generated/client';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 import NextAuth, { type DefaultSession } from 'next-auth';
 import Google from 'next-auth/providers/google';
 import { redirect } from 'next/navigation';
 
+const prisma = new PrismaClient();
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(db),
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
